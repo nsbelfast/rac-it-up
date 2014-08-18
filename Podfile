c75e9d806159f25dfa5757e9dbd0c12b0690b4ee
Iain Wilson <iain.iw.wilson@gmail.com>
2014-08-18 09:53:18 +0100

    Make a login request when the form is submitted

--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ platform :ios, "7.0"
 
 target "RACItUp" do
   pod 'ReactiveCocoa', '2.3.1'
+  pod 'AFNetworking-RACExtensions', '0.1.4'
 end
--- View Controllers/RMBLoginTableViewController.m
+++ View Controllers/RMBLoginTableViewController.m
@@ -8,11 +8,16 @@
 
 #import "RMBLoginTableViewController.h"
 
+#import <RACAFNetworking.h>
+
+#import <UIAlertView+RACSignalSupport.h>
+
 @interface RMBLoginTableViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *loginButton;
 
+@property (nonatomic) AFHTTPRequestOperationManager *client;
 @end
 
 @implementation RMBLoginTableViewController
@@ -37,14 +42,17 @@
   
   @weakify(self);
   self.loginButton.rac_command = [[RACCommand alloc] initWithEnabled:wholeFormIsValid
-                                                         signalBlock:^RACSignal *(id input) {
+                                                         signalBlock:^(UIButton *sender) {
                                                            @strongify(self);
+
                                                            return [self login];
                                                          }];
   
-  RACSignal *keyboardShowingSignal = [RACSignal merge:@[[[NSNotificationCenter.defaultCenter rac_addObserverForName:UIKeyboardWillShowNotification object:nil] mapReplace:@YES],
-                                                        [[NSNotificationCenter.defaultCenter rac_addObserverForName:UIKeyboardDidHideNotification object:nil] mapReplace:@NO]
-                                                        ]];
+  RACSignal *keyboardShowingSignal = [RACSignal merge:@[
+                                       [[NSNotificationCenter.defaultCenter rac_addObserverForName:UIKeyboardWillShowNotification object:nil] mapReplace:@YES],
+                                       [[NSNotificationCenter.defaultCenter rac_addObserverForName:UIKeyboardDidHideNotification object:nil] mapReplace:@NO]
+                                     ]];
+
   [keyboardShowingSignal subscribeNext:^(NSNumber *keyboardIsOnScreen) {
     // look how easy this is! how would we even have done this before?
     NSLog(@"Keyboard is %@showing", keyboardIsOnScreen.boolValue ? @"" : @"not ");
@@ -62,40 +70,49 @@
    */
   
   // 7. Networking & disposables
-  
-  // What can we log into though??
-  
-  /*
-   [[[[client logIn]
-   then:^{
-   return [client loadCachedMessages];
-   }]
-   flattenMap:^(NSArray *messages) {
-   return [client fetchMessagesAfterMessage:messages.lastObject];
-   }]
-   subscribeError:^(NSError *error) {
-   [self presentError:error];
-   } completed:^{
-   NSLog(@"Fetched all messages.");
-   }];
-   */
+
+  self.client = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://httpbin.org/"]];
+  self.client.requestSerializer = [AFJSONRequestSerializer serializer];
+  self.client.responseSerializer = [AFJSONResponseSerializer serializer];
 }
 
-- (RACSignal *)login {
-  return [[RACSignal.empty delay:5] doCompleted:^{
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log in failed"
-                                                    message:@"You suck"
-                                                   delegate:nil
-                                          cancelButtonTitle:@"Sadface"
-                                          otherButtonTitles:@"No, I don't", nil];
-    
-    [alert.rac_buttonClickedSignal subscribeNext:^(NSNumber *buttonIndex) {
-      NSLog(@"Alert clicked button at index: %@", buttonIndex);
-    }];
-    
-    [alert show];
+#pragma mark - Authentication
+
+/// Makes an authenticated request using `credential`
+- (RACSignal *)authenticateWithCredential:(NSURLCredential *)credential {
+  return [[[self.client rac_GET:@"/digest-auth/auth/valid@email.com/password" parameters:nil] initially:^{
+    self.client.credential = credential;
+  }] doError:^(NSError *error) {
+    // Remove the credential if the login failed
+    self.client.credential = nil;
   }];
 }
 
+/// Returns a signal that attempts to login with the entered email / password, displaying an alert on success or error
+- (RACSignal *)login {
+  NSURLCredential *credential = [NSURLCredential credentialWithUser:self.emailTextField.text password:self.passwordTextField.text persistence:NSURLCredentialPersistenceNone];
+
+  return [[[self authenticateWithCredential:credential] catch:^(NSError *error) {
+    NSHTTPURLResponse *response = error.userInfo[AFNetworkingOperationFailingURLResponseErrorKey];
+
+    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Login failed" message:@"" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+
+    RACSignal *displayError = [RACSignal defer:^{
+      [errorAlert show];
+      return [errorAlert.rac_buttonClickedSignal take:1];
+    }];
+
+    if (response.statusCode == 401) {
+      errorAlert.message = @"Invalid email or password";
+    }
+    else {
+      errorAlert.message = @"Unable to login";
+    }
+
+    return [displayError concat:[RACSignal error:error]];
+  }] doCompleted:^{
+    [[[UIAlertView alloc] initWithTitle:@"Login successful" message:@"You're in!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+  }];
+}
 
 @end
